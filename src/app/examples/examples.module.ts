054b93d7b6434ddd87eb6744fd8aef4585bf99a5
idrice24 <idrice.tsafouet@yahoo.com>
2020-09-30 03:01:26 +0100

    Update examples.module.ts

--- src/app/examples/examples.module.ts
+++ src/app/examples/examples.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { LandingComponent } from './landing/landing.component';
 import { ExamplesComponent } from './examples.component';
@@ -9,8 +8,7 @@ import { ExamplesComponent } from './examples.component';
 @NgModule({
     imports: [
         CommonModule,
-        FormsModule,
-        NgbModule
+        FormsModule
     ],
     declarations: [
         LandingComponent,
