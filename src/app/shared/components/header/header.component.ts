5e7c79504d3b0fb8dd4bc48a53a74db2e7675cea
idrice24 <50774032+idrice24@users.noreply.github.com>
2021-08-19 16:10:16 +0100

    update header file #72
    this is to add bar menu items

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
-
+import { MenuItem, PrimeIcons } from 'primeng/api';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+ 
+ // this is to store list of menu items
+   menuItems: MenuItem[];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.loadMenuItems();
   }
+  private loadMenuItems(){
+    this.menuItems = [
+    {
+        label: 'File',
+        items: [{
+          label: 'New',
+          icon: 'pi pi-fw pi-plus',
+          items: [
+            { label: 'Project' },
+            { label: 'Other' },
+          ]
+        },
+        { label: 'Open' },
+        { label: 'Quit' }
+        ]
+      },
+      {
+        label: 'Edit',
+        icon: 'pi pi-fw pi-pencil',
+        items: [
+          { label: 'Delete', icon: 'pi pi-fw pi-trash' },
+          { label: 'Refresh', icon: 'pi pi-fw pi-refresh' }
+        ]
+      }
 
+    ];
+  }
 }
